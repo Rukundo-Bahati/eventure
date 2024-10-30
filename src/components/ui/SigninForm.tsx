07daa7fc6fb4b2abb29d784a1ee7cd1adbbe13a6
Salim Rutaganda <rutagandasalim@gmail.com>
2024-10-30 16:08:02 +0200

    feat: complete login and signup

--- src/components/ui/SigninForm.tsx
+++ src/components/ui/SigninForm.tsx
@@ -1,92 +1,78 @@
-
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import Image from 'next/image'
-import Link from 'next/link'
-import { logIn } from "@/actions/auth"
-import { useSupabaseClient } from '@/lib/supabase/client'
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
-import { LogIn } from "lucide-react"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { logIn } from "@/actions/auth";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export function SigninForm() {
-  const supabase = createClientComponentClient()
+  const supabase = createClientComponentClient();
 
   const handleGoogleSignIn = async () => {
     try {
       const { data, error } = await supabase.auth.signInWithOAuth({
-        provider: 'google',
+        provider: "google",
         options: {
           queryParams: {
-            access_type: 'offline',
-            prompt: 'consent',
+            access_type: "offline",
+            prompt: "consent",
           },
-          redirectTo: `${window.location.origin}/admin/dashboard`
-        }
-      })
-      
-      if (error) throw error
-    } catch (error) {
-      console.error('Error:', error)
-    }
-  }
+          redirectTo: `${window.location.origin}/admin/dashboard`,
+        },
+      });
 
-  
+      if (error) throw error;
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
-    <div className="flex w-full items-center justify-center lg:w-3/5">
-    <div className="w-full max-w-md space-y-6 p-8">
-      <div className="text-center">
-        <h2 className="text-xl font-bold">
-          Event<span className="text-[#7C3AED]">Hive</span>
-        </h2>
-        <h3 className="mt-6 text-2xl font-bold">
-          Sign In to Event Hive
-        </h3>
-      </div>
-      <form action={logIn} className="space-y-6">
-        <div className="space-y-3">
-          <label className="block text-base font-medium">YOUR EMAIL</label>
-          <Input 
-            type="email"
-            name='email'
-            placeholder="Enter your mail" 
-            className="w-full rounded-md border placeholder:text-[#687C94] focus:ring-none focus:border-none bg-white border-gray-200 px-4 py-6"
-          />
+    <div className="flex w-full  items-center justify-center lg:w-3/5">
+      <div className="w-full max-w-md space-y-6 p-8">
+        <div className="text-center">
+          <h2 className="text-xl font-bold">
+            Event<span className="text-[#7C3AED]">Hive</span>
+          </h2>
+          <h3 className="mt-6 text-2xl font-bold">Sign In to Event Hive</h3>
         </div>
-        <div className="space-y-3">
-          <div className="flex justify-between items-center">
-            <label className="block text-base font-medium">PASSWORD</label>
+        <form action={logIn} className="space-y-6 items-center flex flex-col">
+          <div className="space-y-3 w-[500px]">
+            <label className="block text-base font-medium">EMAIL</label>
+            <Input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              className="w-full rounded-md border placeholder:text-[#687C94] focus:ring-none focus:border-none bg-white border-gray-200 px-4 py-6"
+            />
           </div>
-          <Input 
-            type="password" 
-            name="password"
-            placeholder="Enter your password"
-            className="w-full rounded-md border placeholder:text-[#687C94] focus:border-none bg-white border-gray-200 px-4 py-6"
-          />
-        </div>
-        <Button 
-          type="submit" 
-          className="w-full justify-center text-base items-center rounded-md bg-[#7C3AED] py-4 text-white hover:bg-[#6D28D9]"
+          <div className="space-y-3 w-[500px]">
+            <div className="flex justify-between items-center">
+              <label className="block text-base font-medium ">PASSWORD</label>
+            </div>
+            <Input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              className="w-full rounded-md border placeholder:text-[#687C94] focus:border-none bg-white border-gray-200 px-4 py-6"
+            />
+          </div>
+          <Button
+            type="submit"
+            className="w-[250px] mx-auto flex text-center justify-center text-base items-center rounded-md bg-[#7C3AED] py-5 text-white hover:bg-[#6D28D9]"
+          >
+            Sign In
+          </Button>
+        </form>
+        <div className="text-center text-sm text-gray-500">Or</div>
+        <Button
+          variant="outline"
+          onClick={handleGoogleSignIn}
+          className="w-[340px] mx-auto flex items-center justify-center gap-2 rounded-md text-base border py-5"
         >
-          Sign In
+          <Image src="/google.svg" alt="Google" width={20} height={20} />
+          Sign in with Google
         </Button>
-      </form>
-      <div className="text-center text-sm text-gray-500">
-        Or
       </div>
-      <Button  
-        variant="outline" 
-        onClick={handleGoogleSignIn}
-        className="w-full flex items-center justify-center gap-2 rounded-lg border py-3"
-      >
-        <Image 
-          src="/google.svg" 
-          alt="Google" 
-          width={20} 
-          height={20} 
-        />
-        Sign in with Google
-      </Button>
     </div>
-  </div>
-  )
+  );
 }
